9579772a1d78feca027498b77044100ea84c22e2
MAIauuw <mau.ramirezn@duocuc.cl>
2026-05-25 12:19:58 -0400

    Se coloco en controller una conexion para react

--- src/main/java/org/bubbleplat/comunicacion/controller/MensajeController.java
+++ src/main/java/org/bubbleplat/comunicacion/controller/MensajeController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/mensajes")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 public class MensajeController {
 
